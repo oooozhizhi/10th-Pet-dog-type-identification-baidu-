7600b3011d7ca87f398fbc70068769dc30482ef5
oooozhizhi <651482179@qq.com>
2017-09-25 17:47:26 +0800

    new code added

---
+++ sourceCode/picAdd2.py
@@ -0,0 +1,33 @@
+import numpy as np
+import os
+import shutil
+import h5py
+import random
+import inceptionV4
+
+from keras.preprocessing.image import ImageDataGenerator, array_to_img,img_to_array,load_img
+
+datagen = ImageDataGenerator(rotation_range=40,
+							 width_shift_range=0.2,
+      					     height_shift_range=0.2,
+                     		 shear_range=0.2,                    		
+                             zoom_range=0.2,horizontal_flip=True,fill_mode='nearest')
+
+
+
+
+print("2p")
+for dir in os.listdir(r"/home/deeplearning/wh/baiduImage/train_hun"):
+	print(dir)
+	for filename in os.listdir("/home/deeplearning/wh/baiduImage/train_hun/"+dir):
+	    if os.path.exists("/home/deeplearning/wh/baiduImage/train2p_hun/"+dir)==False:
+	         os.mkdir("/home/deeplearning/wh/baiduImage/train2p_hun/"+dir)
+	    shutil.copy("/home/deeplearning/wh/baiduImage/train_hun/"+dir+"/"+filename,"/home/deeplearning/wh/baiduImage/train2p_hun/"+dir+"/"+filename)
+	    img = load_img("/home/deeplearning/wh/baiduImage/train2p_hun/"+dir+"/"+filename)
+	    x = img_to_array(img)  # this is a Numpy array with shape (3, 150, 150)
+	    x = x.reshape((1,) + x.shape)  
+	    i=0
+	    for batch in datagen.flow(x, batch_size=1,save_to_dir="/home/deeplearning/wh/baiduImage/train2p_hun/"+dir+"/", save_prefix='dog', save_format='jpg'):
+	        i += 1
+	        if i > 0:
+	            break  
---
+++ sourceCode/wh-train-7.8.py
@@ -0,0 +1,56 @@
+import h5py
+import numpy as np
+from sklearn.utils import shuffle
+from keras.models import *
+from keras.layers import *
+from keras.utils.np_utils import to_categorical
+import pandas as pd
+from keras.preprocessing.image import *
+np.random.seed(2017)
+
+X_train = []
+X_test = []
+for filename in [ "/home/deeplearning/wh/baiduImage/wh_code/gap_Xception.h5"]:
+    with h5py.File(filename, 'r') as h:
+        X_train.append(np.array(h['train']))
+        y_train = np.array(h['label'])
+
+X_train = np.concatenate(X_train, axis=1)
+y_train = to_categorical(y_train, num_classes=100)
+#X_train, y_train = shuffle(X_train, y_train)
+print(X_train.shape[1:])
+'''
+input_tensor = Input(X_train.shape[1:])
+x = Dense(256,activation='relu')(input_tensor)
+x = Dropout(0.5)(x)
+x = Dense(100, activation='softmax')(x)
+model = Model(input_tensor, x)
+
+model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+model.fit(X_train, y_train, batch_size=16, nb_epoch=30, validation_split=0.2,verbose=True)
+y_pred = model.predict(X_test, verbose=1)
+
+y_class=[]
+for i in range(10593):
+    y_class.append(y_pred[i].argmax())
+
+
+
+gen = ImageDataGenerator()
+test_generator = gen.flow_from_directory("/home/deeplearning/wh/baiduImage/test", (224, 224), shuffle=False, batch_size=16, class_mode=None)
+
+train_generator = gen.flow_from_directory("/home/deeplearning/wh/baiduImage/traindata", (224, 224), shuffle=False, batch_size=16)
+a=train_generator.class_indices
+map={}
+for i in a:
+    map[a[i]]=i.split('_')[1]
+
+re=[]
+for i, fname in enumerate(test_generator.filenames):
+    fname = fname.split('.')[0].split('/')[1]
+    re.append([fname,map[y_class[i]]])
+
+df=pd.DataFrame(re)
+
+df.to_csv('/home/deeplearning/wh/baiduImage/pred.csv', index=None)
+'''
